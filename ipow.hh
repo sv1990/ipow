21e15ae78937a0fe8a9ac905eab3c1546df82cf1
Semir Vrana <semir.vrana@gmx.de>
2020-10-06 20:43:21 +0200

    Optimize: replace branch by array access

--- ipow.hh
+++ ipow.hh
@@ -9,15 +9,16 @@ concept integral = std::is_integral_v<T>;
 template <typename T>
 concept arithmetic = std::is_arithmetic_v<T>;
 
-template <arithmetic F>
-[[nodiscard]] constexpr F ipow(F x, integral auto n) noexcept {
+template <arithmetic F, integral I>
+[[nodiscard]] constexpr F ipow(F x, I n) noexcept {
   if (n == 0) {
     return 1;
   }
-  if (n < 0) {
-    x = F(1) / x;
-    n = -n;
-  }
+  F xs[] = {x, F(1) / x};
+  x      = xs[n < 0];
+  I ns[] = {n, -n};
+  n      = ns[n < 0];
+
   F z = 1;
   while (n != 1) {
     F arr[] = {1, x};
