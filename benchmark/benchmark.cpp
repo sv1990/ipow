31c46175f80f37cc7be21eaf11969c8d194fc4b1
Semir Vrana <semir.vrana@gmx.de>
2020-10-14 10:31:57 +0200

    Use google-benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,56 +1,49 @@
 #include "ipow.hh"
 
+#include <benchmark/benchmark.h>
+
 #include <algorithm>
-#include <chrono>
 #include <cmath>
-#include <iostream>
 #include <random>
 
-template <typename T>
-using uniform_arithmetic_distribution =
-    std::conditional_t<std::is_integral_v<T>, std::uniform_int_distribution<T>,
-                       std::uniform_real_distribution<T>>;
-
-int main(int, char**) {
-  using base_t     = double;
-  using exponent_t = long;
-
-  std::mt19937_64 gen(std::random_device{}());
-  std::vector<base_t> bases;
-  std::generate_n(std::back_inserter(bases), 10000, [&]() mutable {
-    return uniform_arithmetic_distribution<base_t>{0, 10}(gen);
-  });
-
-  std::vector<exponent_t> exponents;
-  std::generate_n(std::back_inserter(exponents), 10000, [&]() mutable {
-    return std::uniform_int_distribution<exponent_t>{
-        std::is_signed_v<exponent_t> ? -20 : 0, 20}(gen);
-  });
-
-  auto t1 = std::chrono::high_resolution_clock::now();
-
-  base_t x1 = 0;
-  for (auto x : bases) {
-    for (auto n : exponents) {
-      x1 += std::pow(x, n);
-    }
-  }
-  auto t2 = std::chrono::high_resolution_clock::now();
-
-  base_t x2 = 0;
-  for (auto x : bases) {
-    for (auto n : exponents) {
-      x2 += ipow::ipow(x, n);
-    }
-  }
-
-  auto t3 = std::chrono::high_resolution_clock::now();
-  std::cout
-      << "  std::pow: " << x1 << ' '
-      << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
-      << "ms\n";
-  std::cout
-      << "ipow::ipow: " << x2 << ' '
-      << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count()
-      << "ms\n";
+const std::vector<double>& get_bases() {
+  static std::mt19937_64 gen(std::random_device{}());
+  static const std::vector<double> bases = [] {
+    std::vector<double> bases;
+    std::generate_n(std::back_inserter(bases), 10000, [&]() mutable {
+      return std::uniform_real_distribution<double>{0, 10}(gen);
+    });
+    return bases;
+  }();
+  return bases;
 }
+
+static void STD_POW(benchmark::State& state) {
+  auto bases = get_bases();
+  auto it    = begin(bases);
+  for (auto _ : state) {
+    auto x = std::pow(*it, state.range(0));
+    ++it;
+    if (it == end(bases)) {
+      it = begin(bases);
+    }
+    benchmark::DoNotOptimize(x);
+  }
+}
+BENCHMARK(STD_POW)->RangeMultiplier(2)->Range(0, 256);
+
+static void IPOW(benchmark::State& state) {
+  auto bases = get_bases();
+  auto it    = begin(bases);
+  for (auto _ : state) {
+    auto x = ipow::ipow(*it, state.range(0));
+    ++it;
+    if (it == end(bases)) {
+      it = begin(bases);
+    }
+    benchmark::DoNotOptimize(x);
+  }
+}
+BENCHMARK(IPOW)->RangeMultiplier(2)->Range(0, 256);
+
+BENCHMARK_MAIN();
